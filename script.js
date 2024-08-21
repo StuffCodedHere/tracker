8a95e5c1b0b66575e36f04b12aec5d2d2d062c92
Omid <130211235+StuffCodedHere@users.noreply.github.com>
2024-08-21 11:15:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 const h1 = document.querySelector("h1")
 
-const initialDate = new Date("2024-07-01")
-
-if (!localStorage.doneCounter) localStorage.setItem("doneCounter", 0)
-let doneCounter = parseInt(localStorage.doneCounter)
-let days = Math.floor((new Date() - initialDate) / 86400000)
-
-document.onclick = () => {
- doneCounter += 2
- localStorage.doneCounter = doneCounter
- h1.innerHTML = days - doneCounter
-}
-
-h1.innerHTML = days - doneCounter
+h1.innerHTML = localStorage.doneCounter
 
 localStorage.removeItem("doneCounter")
